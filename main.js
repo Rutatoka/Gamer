95df636a1ccc8e92ab8402138988b4a2f2c53cb4
Angelina <lunina.angelinka2002@gmail.com>
2021-06-18 17:19:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict"
 let character=document.querySelector(".character");
+
 let map=document.querySelector(".map");
 // let cam=document.querySelector(".camera");
 let x=0;
@@ -14,37 +15,40 @@ const placeCharacter=()=>{
     const held_direction=held_directions[0];
     if(held_direction){
         if(held_direction===directions.right){x+=speed; walk_right();
-            
+            setInterval(() => {
+                mob_walk_left();
+            }, 710);
         }
         if(held_direction===directions.left){x-=speed; walk_left();}
 
         character.setAttribute("facing",held_direction);
-
-    }
     
-    //  if(character.walking===false){ walk_stoper();}
+    }
+
     character.setAttribute("walking",held_direction?"true":"false");
-   
+    
+     // mob.setAttribute("walking",held_direction?"true":"false");
     if(x>650||x<-2){ 
         character.style.transform=false
+
        }
        else{  
            character.style.transform=`translate3d(${x*pixelSize}px,${y*pixelSize}px,0)`;
-      
+             
+          
+        //    mob.style.transform=`translate3d(${x*pixelSize}px,${y*pixelSize}px,0)`;
+        
        }
+
+
    if(x>300||x<-2){
     map.style.transform=false
    }
    else{  
          let camera_left=pixelSize*1;
     map.style.transform=`translate3d(${-x*pixelSize+camera_left}px,${-y*pixelSize}px,0)`;
+    
    }
-   
-
-
-
-     
- 
 
 }
 
@@ -87,38 +91,60 @@ document.addEventListener("keyup",(e)=>{
 function walk_left() {
 
             context.clearRect(0, 0, width, height);
-            drawImage(hero_left, 0, 420);
-       
+            drawImage(hero_left, 0, 452);
+            drawImage(mob_right,110, 452);
            
 }
 function walk_right() {
         context.clearRect(0, 0, width, height);
-        drawImage(hero_right, 0, 420);
-        
+        drawImage(hero_right, 0, 452);
+        drawImage(mob_left, 110, 447);
 }
-function walk_stoper() {
-        context.clearRect(0, 0, width, height);
-        // drawImage(hero_stop, 0, 420);
-        // walk_left()=false;
-        // walk_right()=false;
-}
+// function mob_walk_left() { 
+//      context.clearRect(0, 0, width, height);
+  
+  
+// }
+// function walk_mob_left() {
+//         context.clearRect(0, 0, width, height);
+//         drawImage(mob_left, 500, 420);
+       
+// }
 
+    // context.clearRect(0, 0, width, height);
+    // drawImage(mob_right, 500, 420);
+
+// setInterval(function() {
+//     context.clearRect(0, 0, width, height);
+   
+//     drawImage(mob_right, 100, 120);
+
+
+// }, 200)
+// function timer(){
+//     context.clearRect(0, 0, width, height);
+//     drawImage(mob_left, 100, 120);
+// }
 let context = document.querySelector(".character").getContext("2d");
-let width = 100,
+
+let width = 1100,
     height = 1000;
    
 let hero_left = loadImage("img/hero_walk.png", 64, 160, 7);
-let hero_stop = loadImage("img/hero_stop.png", 65, 260, 1);
+let mob_left = loadImage("img/mob_left.png", 66, 160, 7);
+let mob_right = loadImage("img/mob_right.png", 66, 160, 7);
 let hero_right = loadImage("img/hero_walk_right.png", 64, 160, 7);
 // let girl2 = loadImage("last-guardian-sprites/amg3_lf2.gif", 32, 32, 2);
 
+
+    
 
 
 function drawImage(img, c, v) {
     if (img.num >= img.count) img.num = 1
     else img.num += 1;
     context.drawImage(img.dom, img.width * (img.num - 1), 0, img.width/1.4, img.height/1.3, c-10, v,64, 360);
-}
+   }
 
 function loadImage(path, width, height, count) {
     let image = document.createElement("img");
